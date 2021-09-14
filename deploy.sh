042817390260ffeb61440d7f1bb01c5aebd1d7a3
Zimuzo Obiechina <zimobiechina@gmail.com>
2021-09-14 19:15:03 +0100

    Add deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 # ssh into dev server and create project dir
 gcloud compute ssh --zone=us-central1-c app-server-dev < mkdir nodejs-app && cd nodejs-app
+# get files from repo
 git pull
 npm install --production
 pm2 start app.js
