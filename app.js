795551b2da2b15c0f2e9619fe000a540f81e8680
Zimuzo Obiechina <zimobiechina@gmail.com>
2021-09-14 22:23:41 +0100

    Add deploy script

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
+
+// const PORT = process.env.PORT || 3000;
 
 
 app.get("/", function (req, res) {
