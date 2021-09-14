6001138a5b2c4aa5ad08e38a86ff1e6414ed5130
Zimuzo Obiechina <zimobiechina@gmail.com>
2021-09-14 22:17:10 +0100

    Add deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,12 @@
 
 # ssh into dev server and create project dir
 gcloud compute ssh --zone=us-central1-c app-server-dev 
-cd nodeapp
-# get files from repo
+
 git clone 'git@github.com:Z11mm/jenkins-test-app.git'
+
+cd jenkins-test-app
+
 npm install --production
+
 npm start
 exit
