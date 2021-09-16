c2b1b7f13040f68cdbcef36ce3fd34942b711cfc
Zimuzo Obiechina <zimobiechina@gmail.com>
2021-09-16 07:29:18 +0100

    Edit source code

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get("/", function (req, res) {
   res.send("Hello World, this is working...");
