148f159b5ea7784bdcb11db60bd778f88fc90f05
Zimuzo Obiechina <zimobiechina@gmail.com>
2021-09-17 14:22:47 +0100

    Add deploy script

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 
 app.get("/", function (req, res) {
-  res.send("Hello World" + process.env.NODE_ENV);
+  res.send("Hello World");
 });
 
 app.listen(PORT);
